a7fee3e1552ab947d2f1f234f8cdc9d6f23d8933
Eric Pan <epan717@gmail.com>
2023-10-23 10:25:30 -0700

    Reuse function in fixture

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -4,6 +4,8 @@ import pydian.partials as p
 from pydian import Mapper, get
 from pydian.dicts import drop_keys
 
+import pydash
+
 
 def test_get(simple_data: dict[str, Any]) -> None:
     source = simple_data
@@ -26,6 +28,11 @@ def test_get(simple_data: dict[str, Any]) -> None:
         "CASE_nested_as_list": [source["data"]["patient"]["active"]],
         "CASE_modded": source["data"]["patient"]["id"] + "_modified",
     }
+
+def test_get_new_dsl(simple_data: dict[str, Any]) -> None:
+    source = simple_data
+
+
 
 
 def test_get_index(simple_data: dict[str, Any]) -> None:
@@ -59,6 +66,7 @@ def test_get_index(simple_data: dict[str, Any]) -> None:
         },
     }
 
+
 def test_get_from_list(list_data: list[Any]) -> None:
     source = list_data
     assert get(source, "[*].patient") == [p["patient"] for p in source]
@@ -67,6 +75,7 @@ def test_get_from_list(list_data: list[Any]) -> None:
     assert get(source, "[-1].patient.id") == source[-1]["patient"]["id"]
     assert get(source, "[0:2].patient.id") == [p["patient"]["id"] for p in source[0:2]]
     assert get(source, "[-2:].patient.id") == [p["patient"]["id"] for p in source[-2:]]
+
 
 def test_nested_get(nested_data: dict[str, Any]) -> None:
     source = nested_data
