baeafdbbfabfe594b3d0ea65dc07e7c4dddbae38
Eric Pan <epan717@gmail.com>
2023-10-28 18:16:41 -0700

    Standardize organization of `dicts` tests

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -8,60 +8,33 @@ from pydian.dicts import drop_keys
 def test_get(simple_data: dict[str, Any]) -> None:
     source = simple_data
 
-    def mapping(d: dict[str, Any]) -> dict[str, Any]:
-        return {
-            "CASE_constant": 123,
-            "CASE_single": get(d, "data"),
-            "CASE_nested": get(d, "data.patient.id"),
-            "CASE_nested_as_list": [get(d, "data.patient.active")],
-            "CASE_modded": get(d, "data.patient.id", apply=lambda s: s + "_modified"),
-        }
-
-    mapper = Mapper(mapping, remove_empty=True)
-    res = mapper(source)
-    assert res == {
-        "CASE_constant": 123,
-        "CASE_single": source.get("data"),
-        "CASE_nested": source["data"]["patient"]["id"],
-        "CASE_nested_as_list": [source["data"]["patient"]["active"]],
-        "CASE_modded": source["data"]["patient"]["id"] + "_modified",
-    }
+    assert get(source, "data") == source.get("data")
+    assert get(source, "data.patient.id") == source["data"]["patient"]["id"]
+    assert get(source, "data.patient.active") == source["data"]["patient"]["active"]
+    assert (
+        get(source, "data.patient.id", apply=lambda s: s + "_modified")
+        == source["data"]["patient"]["id"] + "_modified"
+    )
 
 
 def test_get_index(simple_data: dict[str, Any]) -> None:
     source = simple_data
 
-    def mapping(d: dict[str, Any]) -> dict[str, Any]:
-        return {
-            "first": get(d, "list_data[0].patient"),
-            "second": get(d, "list_data[1].patient"),
-            "out_of_bounds": get(d, "list_data[50].patient"),
-            "negative_index": get(d, "list_data[-1].patient"),
-            "slice": {
-                "both": get(d, "list_data[1:3]"),
-                "left": get(d, "list_data[1:]"),
-                "right": get(d, "list_data[:2]"),
-                "all": get(d, "list_data[:]"),
-            },
-        }
-
-    mapper = Mapper(mapping, remove_empty=True)
-    res = mapper(source)
-    assert res == {
-        "first": source["list_data"][0]["patient"],
-        "second": source["list_data"][1]["patient"],
-        "negative_index": source["list_data"][-1]["patient"],
-        "slice": {
-            "both": source["list_data"][1:3],
-            "left": source["list_data"][1:],
-            "right": source["list_data"][:2],
-            "all": source["list_data"][:],
-        },
-    }
+    # Indexing
+    assert get(source, "list_data[0].patient") == source["list_data"][0]["patient"]
+    assert get(source, "list_data[1].patient") == source["list_data"][1]["patient"]
+    assert get(source, "list_data[5000].patient") is None
+    assert get(source, "list_data[-1].patient") == source["list_data"][-1]["patient"]
+    # Slicing
+    assert get(source, "list_data[1:3]") == source["list_data"][1:3]
+    assert get(source, "list_data[1:]") == source["list_data"][1:]
+    assert get(source, "list_data[:2]") == source["list_data"][:2]
+    assert get(source, "list_data[:]") == source["list_data"][:]
 
 
 def test_get_from_list(list_data: list[Any]) -> None:
     source = list_data
+
     assert get(source, "[*].patient") == [p["patient"] for p in source]
     assert get(source, "[*].patient.id") == [p["patient"]["id"] for p in source]
     assert get(source, "[0].patient.id") == source[0]["patient"]["id"]
@@ -73,44 +46,27 @@ def test_get_from_list(list_data: list[Any]) -> None:
 def test_nested_get(nested_data: dict[str, Any]) -> None:
     source = nested_data
 
-    def mapping(d: dict[str, Any]) -> dict[str, Any]:
-        return {
-            "CASE_constant": 123,
-            "CASE_unwrap_active": get(d, "data[*].patient.active"),
-            "CASE_unwrap_id": get(d, "data[*].patient.id"),
-            "CASE_unwrap_list": get(d, "data[*].patient.ints"),
-            "CASE_unwrap_list_twice": get(d, "data[*].patient.ints", flatten=True),
-            "CASE_unwrap_list_dict": get(d, "data[*].patient.dicts[*].num"),
-            "CASE_unwrap_list_dict_twice": get(d, "data[*].patient.dicts[*].num", flatten=True),
-            # Expect this to get removed
-            "CASE_bad_key": {
-                "single": get(d, "missing.key"),
-                "unwrap": get(d, "missing[*].key"),
-                "unwrap_twice": get(d, "missing[*].key[*].here"),
-                "overindex": get(d, "data[8888].patient"),
-            },
-        }
-
-    mapper = Mapper(mapping, remove_empty=True)
-    res = mapper(source)
-    assert res == {
-        "CASE_constant": 123,
-        "CASE_unwrap_active": [True, False, True, True],
-        "CASE_unwrap_id": ["abc123", "def456", "ghi789", "jkl101112"],
-        "CASE_unwrap_list": [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-        "CASE_unwrap_list_twice": [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        "CASE_unwrap_list_dict": [[1, 2], [3, 4], [5, 6], [7]],
-        "CASE_unwrap_list_dict_twice": [1, 2, 3, 4, 5, 6, 7],
-    }
+    assert get(source, "data[*].patient.active") == [True, False, True, True]
+    assert get(source, "data[*].patient.id") == ["abc123", "def456", "ghi789", "jkl101112"]
+    assert get(source, "data[*].patient.ints") == [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    assert get(source, "data[*].patient.ints", flatten=True) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    assert get(source, "data[*].patient.dicts[*].num") == [[1, 2], [3, 4], [5, 6], [7]]
+    assert get(source, "data[*].patient.dicts[*].num", flatten=True) == [1, 2, 3, 4, 5, 6, 7]
+    assert get(source, "missing.key") is None
+    assert get(source, "missing[*].key") is None
+    assert get(source, "missing[*].key[*].here") is None
+    assert get(source, "data[8888].patient") is None
 
 
 def test_drop_keys(nested_data: dict[str, Any]) -> None:
     source = nested_data
+
     keys_to_drop = {
         "data[0].patient",
         "data[2].patient.id",
         "data[3].patient.active",
     }
+
     res = drop_keys(source, keys_to_drop)
     for k in keys_to_drop:
         assert get(res, k) is None
@@ -118,46 +74,47 @@ def test_drop_keys(nested_data: dict[str, Any]) -> None:
 
 def test_get_apply(simple_data: dict[str, Any]) -> None:
     source = simple_data
+
     OLD_STR, NEW_STR = "456", "FourFiveSix"
     single_apply = str.upper
     chained_apply = [str.upper, p.do(str.replace, OLD_STR, NEW_STR)]
     failed_chain_apply = [str.upper, lambda _: None, p.do(str.replace, OLD_STR, NEW_STR)]
-    res = {
-        "single_apply": get(source, "data.patient.id", apply=single_apply),
-        "chained_apply": get(source, "list_data[0].patient.id", apply=chained_apply),
-        "failed_chained_apply": get(source, "list_data[0].patient.id", apply=failed_chain_apply),
-        "not_found": get(source, "data.notFoundKey", apply=chained_apply),
-    }
-    assert res == {
-        "single_apply": str.upper(source["data"]["patient"]["id"]),
-        "chained_apply": (str.upper(source["list_data"][0]["patient"]["id"])).replace(
-            OLD_STR, NEW_STR
-        ),
-        "failed_chained_apply": None,
-        "not_found": None,
-    }
+
+    assert get(source, "data.patient.id", apply=single_apply) == str.upper(
+        source["data"]["patient"]["id"]
+    )
+    assert get(source, "list_data[0].patient.id", apply=chained_apply) == (
+        str.upper(source["list_data"][0]["patient"]["id"])
+    ).replace(OLD_STR, NEW_STR)
+    assert get(source, "list_data[0].patient.id", apply=failed_chain_apply) is None
+    assert get(source, "data.notFoundKey", apply=chained_apply) is None
 
 
 def test_get_only_if(simple_data: dict[str, Any]) -> None:
     source = simple_data
+
     KEY = "data.patient.id"
     passes_check = get(source, KEY, only_if=lambda s: str.startswith(s, "abc"), apply=str.upper)
     fails_check = get(source, KEY, only_if=lambda s: str.startswith(s, "000"), apply=str.upper)
+
     assert passes_check == source["data"]["patient"]["id"].upper()
     assert fails_check is None
 
 
 def test_get_single_key_tuple(simple_data: dict[str, Any]) -> None:
     source = simple_data
+
     assert get(source, "data.patient.[id,active]") == [
         source["data"]["patient"]["id"],
         source["data"]["patient"]["active"],
     ]
+
     # Handle tuple case
     assert get(source, "data.patient.(id,active)") == (
         source["data"]["patient"]["id"],
         source["data"]["patient"]["active"],
     )
+
     # Allow whitespace within brackets
     assert get(source, "data.patient.[id, active]") == get(source, "data.patient.[id,active]")
     assert get(source, "data.patient.[id,active,missingKey]") == [
@@ -165,6 +122,7 @@ def test_get_single_key_tuple(simple_data: dict[str, Any]) -> None:
         source["data"]["patient"]["active"],
         None,
     ]
+
     # Expect list unwrapping to still work
     assert get(source, "list_data[*].patient.[id, active]") == [
         [p["patient"]["id"], p["patient"]["active"]] for p in source["list_data"]
@@ -192,7 +150,7 @@ def test_get_single_key_tuple(simple_data: dict[str, Any]) -> None:
     ]
 
     # Test only_if filtering
-    assert get(source, "data.patient.[id, active, missingKey]", only_if=lambda _: False) == None
+    assert get(source, "data.patient.[id, active, missingKey]", only_if=lambda _: False) is None
 
 
 def test_get_nested_key_tuple(nested_data: dict[str, Any]) -> None:
