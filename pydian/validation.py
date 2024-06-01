a81cafb43fd9fdddd85b7f4de7bcb33e375363bd
Eric Pan <eric.pan@stanford.edu>
2024-05-31 20:27:51 -0700

    Convert `RuleSet` into `RuleGroup` (a list)
    A `list` maintains the order of the rules (which will hopefully make it
    easier to debug). The tradeoff is lookup which generally shouldn't be a
    problem (i.e. we expect small `RuleGroup`s at this point).
    
    Ergonomics > performance at this stage!

--- pydian/validation.py
+++ pydian/validation.py
@@ -4,14 +4,14 @@ from pydantic import create_model
 from result import Err, Ok
 
 from .dicts import get
-from .rules import Rule, RuleSet
+from .rules import Rule, RuleGroup
 
 
 def validate(
-    source: dict[str, Any], validation_map: dict[str, Rule | RuleSet]
-) -> Ok[dict] | Err[RuleSet]:
+    source: dict[str, Any], validation_map: dict[str, Rule | RuleGroup]
+) -> Ok[dict] | Err[RuleGroup]:
     """
-    Performs valiudation on the `source` dict. Enforces corresponding `Rule`s and `RuleSet`s
+    Performs valiudation on the `source` dict. Enforces corresponding `Rule`s and `RuleGroup`s
       at a given key
     """
 
@@ -27,21 +27,21 @@ def validate(
             failed_rules.append(r_rs)
 
     if failed_rules:
-        return Err(RuleSet(failed_rules))
+        return Err(RuleGroup(failed_rules))
 
     return Ok(source)
 
 
 # TODO: Implement this and add to pydantic test
 
-# def to_pydantic(validation_map: dict[str, Rule | RuleSet]) -> dict[str, Any]:
+# def to_pydantic(validation_map: dict[str, Rule | RuleGroup]) -> dict[str, Any]:
 #     """
 #     Convert a validation_map to a Pydantic model.
 
 #     OK. Try the following algo:
 #     1. First-pass of validation_map:
 #         - Parse-out structural schema
-#         - Parse-out custom Rule/RuleSet requirements
+#         - Parse-out custom Rule/RuleGroup requirements
 #     2. Use pydantic `create_model` to create custom class at runtime
 #         - Validate `source` against custom class
 #     3. Run additional rules from first-pass
