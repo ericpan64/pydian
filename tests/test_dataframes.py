6aa40e3a29051208c5e6c5518cb79e2c3aca52f4
Eric Pan <eric.pan@stanford.edu>
2024-07-28 10:27:35 -0700

    Rename `outer_join` -> `left_join`

--- tests/test_dataframes.py
+++ tests/test_dataframes.py
@@ -6,7 +6,7 @@ from polars.testing import (
 )
 from result import Err
 
-from pydian.dataframes import inner_join, outer_join, select, union
+from pydian.dataframes import inner_join, left_join, select, union
 
 
 def test_select(simple_dataframe: pl.DataFrame) -> None:
@@ -159,28 +159,28 @@ def test_outer_join(simple_dataframe: pl.DataFrame) -> None:
 
     # `None` cases
     assert isinstance(
-        outer_join(source, df_right, on="d"), Err
+        left_join(source, df_right, on="d"), Err
     ), "Expected Err since `d` is not in right"
     assert isinstance(
-        outer_join(source, df_right, on="e"), Err
+        left_join(source, df_right, on="e"), Err
     ), "Expected Err since `e` is not in left"
     assert isinstance(
-        outer_join(source, df_right, on="f"), Err
+        left_join(source, df_right, on="f"), Err
     ), "Expected Err since `f` is not in either"
     assert isinstance(
-        outer_join(source, df_right, on=["a", "f"]), Err
+        left_join(source, df_right, on=["a", "f"]), Err
     ), "Expected Err since `f` is not in either"
     assert isinstance(
-        outer_join(source, df_right, on=["e", "f"]), Err
+        left_join(source, df_right, on=["e", "f"]), Err
     ), "Expected Err since `f` is not in either"
     assert isinstance(
-        outer_join(source, df_right, on=["a", "e"]), Err
+        left_join(source, df_right, on=["a", "e"]), Err
     ), "Expected Err since `e` is not in left"
 
     # Basic join
     expected = deepcopy(source)
     expected = expected.join(df_right, on="a", how="left")
-    result = outer_join(source, df_right, on="a")
+    result = left_join(source, df_right, on="a")
 
     assert_frame_equal(result, expected)  # type: ignore
 
@@ -188,11 +188,11 @@ def test_outer_join(simple_dataframe: pl.DataFrame) -> None:
     df_empty_right = pl.DataFrame(
         {"a": pl.Series([], dtype=pl.Int64), "e": pl.Series([], dtype=pl.Int64)},
     )
-    result = outer_join(source, df_empty_right, on="a")
+    result = left_join(source, df_empty_right, on="a")
     assert isinstance(result, Err), f"Expected Err -- resulting DataFrame is empty, got: {result}"
 
     # # Test `consume=True`
-    # result = outer_join(source, df_right, on="a", consume=True)
+    # result = left_join(source, df_right, on="a", consume=True)
     # assert expected.equals(result)
     # assert df_right.equals(pl.DataFrame({
     #     "a": [6, 7],
