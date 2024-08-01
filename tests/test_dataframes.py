463cd5b51498daf0e83a8b16b2bc207eafdb7d57
Eric Pan <eric.pan@stanford.edu>
2024-08-01 10:36:53 -0700

    Add `order_by` function

--- tests/test_dataframes.py
+++ tests/test_dataframes.py
@@ -6,12 +6,12 @@ from polars.testing import (
 )
 from result import Err
 
-from pydian.dataframes import join, select, union
+from pydian.dataframes import group_by, join, select, union
 
 
 def test_select(simple_dataframe: pl.DataFrame) -> None:
     source = simple_dataframe
-    select(source, "a")
+
     assert_frame_equal(select(source, "a"), source[["a"]])  # type: ignore
     assert_frame_equal(source[["a", "b"]], select(source, "a, b"))  # type: ignore
     assert_frame_equal(select(source, "*"), source)  # type: ignore
@@ -31,12 +31,6 @@ def test_select(simple_dataframe: pl.DataFrame) -> None:
     assert_frame_equal(q1, source.filter(pl.col("a") == 0).select("b"))  # type: ignore
     assert_frame_equal(q2, source.filter(pl.col("a") % 2 == 0).select(["a", "b", "c"]))  # type: ignore
     assert isinstance(q3_err, Err)
-
-    # "First n"
-    # assert_frame_equal(select(source, "*", apply=p.keep(5)), source.head(5))  # type: ignore
-
-    # # Distinct
-    # assert_frame_equal(select(source, "*", apply=p.distinct()), source.drop_duplicates())  # type: ignore
 
 
 def test_select_consume(simple_dataframe: pl.DataFrame) -> None:
@@ -235,12 +229,6 @@ def test_union(simple_dataframe: pl.DataFrame) -> None:
     result = union(simple_dataframe, rows_to_union)
     pl.DataFrame(expected_data).equals(result)  # type: ignore
 
-    # # Test consume functionality
-    # rows_df = pl.DataFrame(rows_to_union)
-    # result = union(simple_dataframe, rows_df, consume=True)
-    # pl.DataFrame(expected_data).equals(result)
-    # assert rows_df.is_empty(), f"Expected rows_df to be empty, but got {rows_df}"
-
     # Test default value functionality
     rows_to_union_default = [{"a": 7, "b": "i"}]
     expected_data_default = {
@@ -256,3 +244,72 @@ def test_union(simple_dataframe: pl.DataFrame) -> None:
     incompatible_rows = [{"e": 8}]
     result = union(simple_dataframe, incompatible_rows)
     assert isinstance(result, Err), f"Expected None, but got {result}"
+
+
+def test_group_by(simple_dataframe: pl.DataFrame) -> None:
+    # Group column -- default aggregation (`.all()`)
+    group_by_a = group_by(simple_dataframe, "a")  # (`a` has all unique int values)
+    group_by_b = group_by(simple_dataframe, "b")  # (`b` has all unique str values)
+    group_by_c = group_by(simple_dataframe, "c")  # (`c` has half `True`, half `False`)
+    group_by_d = group_by(simple_dataframe, "d")  # (`d` has all `None`)
+
+    assert_frame_equal(group_by_a, simple_dataframe.group_by("a", maintain_order=True).all())  # type: ignore
+    assert_frame_equal(group_by_b, simple_dataframe.group_by("b", maintain_order=True).all())  # type: ignore
+    assert_frame_equal(group_by_c, simple_dataframe.group_by("c", maintain_order=True).all())  # type: ignore
+    assert_frame_equal(group_by_d, simple_dataframe.group_by("d", maintain_order=True).all())  # type: ignore
+
+    # Group by multiple columns -- default aggregation (`.all()`)
+    group_by_ab = group_by(simple_dataframe, "a, b")
+    assert_frame_equal(
+        group_by_ab, simple_dataframe.group_by(["a", "b"], maintain_order=True).all()  # type: ignore
+    )
+
+    group_by_ac = group_by(simple_dataframe, "a, c")
+    assert_frame_equal(
+        group_by_ac, simple_dataframe.group_by(["a", "c"], maintain_order=True).all()  # type: ignore
+    )
+
+    # Group by with `len()` aggregation
+    group_by_a_len = group_by(simple_dataframe, "a -> ['*'.len()]")
+    assert_frame_equal(
+        group_by_a_len,  # type: ignore
+        simple_dataframe.group_by("a", maintain_order=True).agg(
+            [pl.col("*").len().name.suffix("_len")]
+        ),
+    )
+
+    # Group by with `sum()` aggregation
+    group_by_a_sum = group_by(simple_dataframe, "b -> ['a'.sum()]")
+    assert_frame_equal(
+        group_by_a_sum,  # type: ignore
+        simple_dataframe.group_by("b", maintain_order=True).agg([pl.col("a").sum().alias("a_sum")]),
+    )
+
+    # Group by with `mean()` aggregation
+    group_by_a_mean = group_by(simple_dataframe, "b -> ['a'.mean()]")
+    assert_frame_equal(
+        group_by_a_mean,  # type: ignore
+        simple_dataframe.group_by("b", maintain_order=True).agg(
+            [pl.col("a").mean().alias("a_mean")]
+        ),
+    )
+
+    # Group by with multiple aggregations
+    group_by_a_aggs = group_by(
+        simple_dataframe, "c -> ['a'.sum(), 'a'.mean(), 'a'.min(), 'a'.max()]"
+    )
+    assert_frame_equal(
+        group_by_a_aggs,  # type: ignore
+        simple_dataframe.group_by("c", maintain_order=True).agg(
+            [
+                pl.col("a").sum().alias("a_sum"),
+                pl.col("a").mean().alias("a_mean"),
+                pl.col("a").min().alias("a_min"),
+                pl.col("a").max().alias("a_max"),
+            ]
+        ),
+    )
+
+    # Test error handling
+    assert isinstance(group_by(simple_dataframe, "a -> b"), Err)
+    assert isinstance(group_by(simple_dataframe, "a -> ['b'.invalid_agg()]"), Err)
