cf62d3abcc3ede7ce24e6529984fb2b6ed6db9fa
Eric Pan <eric.pan@stanford.edu>
2024-12-13 16:28:29 -0800

    Rename `validate.py` -> `core.py` bc neatness

--- pydian/validation/__init__.py
+++ pydian/validation/__init__.py
@@ -1,13 +1,13 @@
+from .core import validate
 from .rules import RC, RGC, Rule, RuleGroup
 from .specific import InRange, InSet, IsOptional, IsRequired, IsType, MaxCount, MinCount
-from .validate import validate
 
 __all__ = [
     "RC",
     "RGC",
     "Rule",
     "RuleGroup",
-    "validate",
+    "core",
     "IsOptional",
     "IsRequired",
     "InRange",
